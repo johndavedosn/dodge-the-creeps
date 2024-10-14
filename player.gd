11fbd9e42a01c948284c1e92761b4004f5d0c2b1
Evilcoder <moumenisbest@gmail.comr>
2024-10-14 23:13:35 +0100

    inital

---
+++ player.gd
@@ -0,0 +1,37 @@
+extends Area2D
+signal hit
+
+@export var speed = 400
+var screen_size
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+func start(pos):
+	position = pos
+	$CollisionShape2D.disabled = false
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:	
+		$AnimatedSprite2D.stop()
+	position += velocity * delta
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = false
+func _on_body_entered(body: Node2D) -> void:
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
